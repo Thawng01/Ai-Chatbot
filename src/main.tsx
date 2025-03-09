e3b186f66f5c1d4db3f95ddd3910debe824daa25
thechin press <thechinpress@gmail.com>
2025-03-10 00:35:35 +0630

    fixed

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App.tsx";
 import AuthContextProvider from "./context/AuthContextProvider.tsx";
 import { RouterProvider } from "react-router";
-import ProtectedRoute from "./ProtectedRoute.tsx";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>

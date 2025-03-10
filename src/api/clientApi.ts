7ab791af882ae3ddba13a16cc321401c92f9fe3f
thechin press <thechinpress@gmail.com>
2025-03-11 01:27:19 +0630

    change url

--- src/api/clientApi.ts
+++ src/api/clientApi.ts
@@ -3,7 +3,7 @@ import axios from "axios"
 export const url = "https://ai-chatbot-api-5rm8.onrender.com"
 export const baseURL = "http://localhost:5000"
 export const clientApi = axios.create({
-    baseURL: baseURL,
+    baseURL: url,
     headers: {
         "Content-Type": "application/json",
     },
